465bc3353d481d432c06f1dceb018413eb93f3eb
MP_Herrera <153849807+MPHerreraa@users.noreply.github.com>
2024-04-15 21:33:36 -0300

    Desafío 1 terminado

---
+++ index.js
@@ -0,0 +1,9 @@
+let prod1 = "Red velvet"
+let precioU = 20000
+
+alert ("Hola, estás por comprar el producto: " + prod1 + "cuyo precio es " + precioU)
+let cantidad_deseada = prompt("Ingrese la cantidad que quiere comprar: ")
+
+const costo_total = precioU * cantidad_deseada 
+
+alert ("Usted compró " + cantidad_deseada + " " + prod1 + ". El costo total es $" + costo_total)
